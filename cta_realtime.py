a2a6c5bc73f1fa150d062a9b3e9a7cbe5c66335f
hailey <188331+haileyplusplus@users.noreply.github.com>
2024-12-04 13:04:29 -0600

    Interpolation works with debug prints and interpolation warning

--- cta_realtime.py
+++ cta_realtime.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 import sys
 import datetime
 from pathlib import Path
@@ -22,11 +24,11 @@ class FeedWrapper:
         self.cache[route] = rv
         return rv
 
-    def get_stop_patterns(self, route):
-        stats = self.feed.compute_trip_stats(route)
+    def get_stop_patterns(self, route: str):
+        stats = self.feed.compute_trip_stats([route])
         stop_patterns = stats.groupby(['stop_pattern_name'])
         # g.iloc[(g['distance'] - key).abs().argsort()][:1]
-        return stop_patterns
+        return stop_patterns.first()
 
     def get_closest_pattern(self, route, dist):
         patterns = self.get_stop_patterns(route)
@@ -52,25 +54,68 @@ class RealtimeConverter:
         rtdf = pd.read_csv(rtfile, low_memory=False)
         self.days[self.start] = rtdf
 
+    def process_trip(self, date, route, pid, sched_stops, single_rt_trip):
+        print(f'  -- process trip -- ')
+        # this interpolation isn't quite right: maybe need to set the index and use that
+        # fixed with below
+        # pd.concat([spf, s3], ignore_index=True).sort_values(['pdist']).interpolate(method='linear')[1:].astype(int)[:50]
+        # sp['unixts'] = sp.apply(lambda x: int(datetime.datetime.strptime(x.tmstmp, '%Y%m%d %H:%M').timestamp()), axis=1)
+        # intermediate2 = pd.concat([spf, s3], ignore_index=True).sort_values(['pdist']).set_index('pdist').interpolate(method='index')[1:].astype(int)
+        # trip_stops from gtfs
+        def timefn(x):
+            # TODO: figure out 24-hour cutover
+            rawdate = datetime.datetime.strptime(x, '%Y%m%d %H:%M')
+            if rawdate.hour < 4:
+                rawdate += datetime.timedelta(days=1)
+            return int(rawdate.timestamp())
+        times = single_rt_trip['tmstmp'].apply(timefn)
+        single_rt1 = pd.concat([times, single_rt_trip['pdist']], axis=1)
+        deltas = single_rt1.iloc[1] - single_rt1.iloc[0]
+        if single_rt1.iloc[0].pdist == 0:
+            single_rt = single_rt1
+        else:
+            v = deltas.pdist / deltas.tmstmp
+            nt = int(single_rt1.iloc[0].tmstmp - single_rt1.iloc[0].pdist / v)
+            fakerow = pd.DataFrame([{'tmstmp': nt, 'pdist': 0}])
+            single_rt = pd.concat([fakerow, single_rt1]).sort_values(['pdist'], ignore_index=True)
+        single_rt['stop_id'] = -1
+        single_sched = pd.concat([sched_stops['shape_dist_traveled'].rename('pdist'), sched_stops['stop_id']],
+                                 axis=1).reset_index().drop(columns=['index'])
+        print(single_rt)
+        print(single_sched)
+        print(' ====== ')
+        combined = pd.concat([single_rt, single_sched],
+                             ignore_index=True).sort_values(
+            ['pdist']).set_index('pdist')
+        print(combined)
+        interpolated = combined.interpolate(method='index')[1:].astype(int)
+        return interpolated
+
     def process_pattern(self, date, route, pid):
-        df = self.days.get(date)
-        if not df:
+        df: pd.DataFrame = self.days.get(date)
+        if df.empty:
             return False
-        pdf = df.query(f'route == "{route}" and pid == {pid}')
+        pdf = df.query(f'rt == "{route}" and pid == {pid}')
         approx_len = pdf.pdist.max()
         schedule_patterns = self.fw.get_closest_pattern(route, approx_len)
         representative_trip = schedule_patterns.iloc[0].trip_id
-        stops = self.fw.get_trip_stops(representative_trip)
+        sched_stops = self.fw.get_trip_stops(representative_trip)
         rt_trips = pdf.tatripid.unique()
         for t in rt_trips:
-            pass
-        # this interpolation isn't quite right: maybe need to set the index and use that
-        # pd.concat([spf, s3], ignore_index=True).sort_values(['pdist']).interpolate(method='linear')[1:].astype(int)[:50]
-        # sp['unixts'] = sp.apply(lambda x: int(datetime.datetime.strptime(x.tmstmp, '%Y%m%d %H:%M').timestamp()), axis=1)
-
-
-
+            single_rt_trip = pdf[pdf.tatripid == t]
+            r = self.process_trip(date, route, pid, sched_stops, single_rt_trip)
+            print(r)
 
 
 if __name__ == "__main__":
-    pass
+    print(f'Starting')
+    sched_path = Path('~/datasets/transit').expanduser()
+    feed = gtfs_kit.read_feed(sched_path / 'google_transit_2024-05-18.zip', 'mi')
+    fw = FeedWrapper(feed, '20240509')
+    start = datetime.date(2024, 5, 9)
+    rt = RealtimeConverter(Path('~/tmp/transit').expanduser(),
+                           fw,
+                           start,
+                           start)
+    rt.process()
+    rt.process_pattern(start, '72', 10918)

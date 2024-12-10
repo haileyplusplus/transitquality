d02a80e903b7abf2024848eed6c2cdf5a38ef7b6
hailey <188331+haileyplusplus@users.noreply.github.com>
2024-12-10 10:20:35 -0600

    Update testing utility

--- schedtest.py
+++ schedtest.py
@@ -2,10 +2,14 @@ import sys
 from pathlib import Path
 
 import gtfs_kit
+import transit_service_analyst as tsa
 
 
 if __name__ == "__main__":
     files = Path('.').glob('cta_rt*.zip')
     for f in files:
         feed = gtfs_kit.read_feed(f, dist_units='mi')
+        ds = feed.calendar_dates.iloc[0].date
+        print(f'First date: {ds}')
+        #analysis = tsa.load_gtfs('gtfs', ds)
         break

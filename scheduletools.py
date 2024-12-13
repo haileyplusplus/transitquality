17bc7ca99d1d09ab1a6ce38dea4d33c3bb4391e5
hailey <188331+haileyplusplus@users.noreply.github.com>
2024-12-13 14:14:28 -0600

    Allow tools to output daily service span summary

--- scheduletools.py
+++ scheduletools.py
@@ -8,6 +8,7 @@ from pathlib import Path
 import gtfs_kit
 import pendulum
 import pandas as pd
+import tqdm
 
 
 class DateWrap:
@@ -24,11 +25,17 @@ class DateWrap:
     def __str__(self):
         return self.datestr
 
+    def __lt__(self, other):
+        return self.date < other.date
+
     def get(self):
         return self.date
 
     def tomorrow(self):
         return DateWrap(self.date.add(days=1).strftime('%Y%m%d'))
+
+    def in_past(self):
+        return self.date < pendulum.now().date()
 
 
 class TofdWrap:
@@ -66,6 +73,7 @@ class TofdWrap:
 
 class ScheduleTools:
     def __init__(self, schedule_dir: Path):
+        self.schedule_dir = schedule_dir
         sched = sorted([x for x in schedule_dir.glob('cta_20??????.zip')])[-1]
         self.feed = gtfs_kit.read_feed(sched, dist_units='mi')
         self.cache = {}
@@ -74,16 +82,22 @@ class ScheduleTools:
         result = self.cache.get(date.get())
         if result is not None:
             return result
-        #routes = self.feed.get_routes(str(date))
-        #route_ids = list(routes.route_id)
-        route_ids = ['8', '72', '74', '37']
+        routes = self.feed.get_routes(str(date))
+        if routes.empty:
+            return None
+        route_ids = list(routes.route_id)
+        #route_ids = ['8', '72', '74', '37']
         cached = self.feed.compute_trip_stats(route_ids)
         self.cache[date.get()] = cached
         return cached
 
     def correct_route_stats(self, date: DateWrap):
         today = self.get_daily_stats(date)
+        if today is None:
+            return None
         tomorrow = self.get_daily_stats(date.tomorrow())
+        if tomorrow is None:
+            return None
         for col in {'start_time', 'end_time'}:
             today[col] = today.apply(lambda x: TofdWrap(date, x[col]), axis=1)
             tomorrow[col] = tomorrow.apply(lambda x: TofdWrap(date.tomorrow(), x[col]), axis=1)
@@ -96,10 +110,25 @@ class ScheduleTools:
         return combined
 
     def route_stats(self, date: DateWrap, nodrop=False):
-        stats = self.feed.compute_route_stats(self.correct_route_stats(date), [str(date)], split_directions=True)
+        crs = self.correct_route_stats(date)
+        if crs is None:
+            return None
+        stats = self.feed.compute_route_stats(crs, [str(date)], split_directions=True)
         if nodrop:
             return stats
         return stats.drop(columns=(['route_short_name', 'num_trip_starts', 'num_trip_ends', 'num_stop_patterns', 'is_loop', 'route_type']))
+
+    def generate(self):
+        today = pendulum.now().date().strftime('%Y%m%d')
+        dates = self.feed.get_dates()
+        #dates = ['20241230', '20241231']
+        sched_dates = [DateWrap(x) for x in dates if not DateWrap(x).in_past()]
+        df = pd.DataFrame()
+        for d in tqdm.tqdm(sorted(sched_dates)):
+            r = self.route_stats(d)
+            if r is not None:
+                df = pd.concat([df, r])
+        df.to_json(self.schedule_dir / f'route-summary-{today}.json')
 
 
 if __name__ == "__main__":
@@ -111,3 +140,4 @@ if __name__ == "__main__":
     args = parser.parse_args()
     sched_dir = Path(args.schedule_dir[0]).expanduser()
     tools = ScheduleTools(sched_dir)
+    tools.generate()

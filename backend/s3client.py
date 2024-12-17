d8d66c7e2ec70256ced95b79c41165af1d8919be
hailey <188331+haileyplusplus@users.noreply.github.com>
2024-12-16 22:52:11 -0600

    Add simple S3 client

---
+++ backend/s3client.py
@@ -0,0 +1,31 @@
+import datetime
+
+import boto3
+import sys
+import json
+
+
+class S3Client:
+    BUCKET_NAME = 'transitquality2024'
+
+    def __init__(self):
+        self.client = boto3.client()
+        #self.s3 = boto3.resource('s3')
+        #self.bucket = self.s3.Bucket('transitquality2024')
+
+    def write_api_response(self, ts: datetime.datetime, command: str, contents: str):
+        day = ts.strftime('%Y%m%d')
+        full = ts.strftime('%H%M%Sz')
+        key = f'bustracker/raw/{command}/{day}/t{full}.json'
+        response = self.client.put_object(
+            Bucket=self.BUCKET_NAME,
+            Body=contents.encode('utf-8'),
+            Key=key
+        )
+        return response
+
+
+if __name__ == "__main__":
+    # test
+    c = S3Client()
+    c.write_api_response(datetime.datetime.now(), 'test', json.dumps({'test-response': sys.argv[1]}))

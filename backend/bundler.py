2de40721df25b692a6127fe0251d7462dd849d25
hailey <188331+haileyplusplus@users.noreply.github.com>
2024-12-29 17:54:00 -0600

    Compress and scan all files

--- backend/bundler.py
+++ backend/bundler.py
@@ -1,12 +1,15 @@
 #!/usr/bin/env python3
 
 import argparse
+import bz2
 import datetime
 import json
 from pathlib import Path
 
 
 class Bundler:
+    THRESH = datetime.timedelta(minutes=5)
+
     def __init__(self, data_dir: Path, day: str):
         self.data_dir = data_dir
         self.day = day.replace('-', '')
@@ -17,6 +20,7 @@ class Bundler:
         self.index = 0
         self.route_index = {}
         self.requests_out = []
+        self.outfile = self.data_dir / f'bundle-{self.day}.json.bz2'
 
     def store_routes(self, routes, request_time: datetime.datetime):
         for rt in routes.split(','):
@@ -25,21 +29,24 @@ class Bundler:
         self.index += 1
 
     def output(self):
-        outfile = self.data_dir / f'bundle-{self.day}.json'
-        if outfile.exists():
+        if not self.complete(thresh=self.THRESH):
+            print(f'Not bundling incomplete day {self.day}')
+            return False
+        if self.outfile.exists():
             print(f'Not overwriting existing file')
             return False
-        with open(outfile, 'w') as jfh:
+        with bz2.open(self.outfile, 'wt', encoding='UTF-8') as jfh:
             outdict = {'v': '2.0',
-                       'command': 'vehiclebundle',
+                       'bundle_type': 'vehicle',
                        'day': self.day,
                        'first': self.first.isoformat(),
                        'last': self.last.isoformat(),
+                       'max_interval_seconds': self.max_interval.total_seconds(),
                        'index': self.route_index,
                        'requests': self.requests_out}
             json.dump(outdict, jfh)
 
-    def summary(self):
+    def summary(self, by_route=False):
         print(f'Summary for {self.day}:')
         print()
         print(f'First update: {self.first.isoformat()}')
@@ -47,9 +54,21 @@ class Bundler:
         print(f'Max interval: {self.max_interval.total_seconds()} seconds')
         print(f'Total requests: {len(self.requests_out)}')
         print()
-        print(f'Requests by route:')
-        for k, v in sorted(self.route_index.items()):
-            print(f'  {k:4}: {len(v):5d}')
+        if by_route:
+            print(f'Requests by route:')
+            for k, v in sorted(self.route_index.items()):
+                print(f'  {k:4}: {len(v):5d}')
+
+    def complete(self, thresh: datetime.timedelta):
+        day_start = datetime.datetime.strptime(self.day, '%Y%m%d').replace(tzinfo=datetime.UTC)
+        day_end = day_start + datetime.timedelta(days=1)
+        if self.max_interval > thresh:
+            return False
+        if self.first - day_start > thresh:
+            return False
+        if day_end - self.last > thresh:
+            return False
+        return True
 
     def scan_file(self, file: Path):
         with open(file) as fh:
@@ -74,6 +93,8 @@ class Bundler:
         return True
 
     def scan_day(self):
+        if self.outfile.exists():
+            return False
         dir_ = self.data_dir / 'getvehicles' / self.day
         if not dir_.exists():
             print(f'Directory not found')
@@ -86,6 +107,16 @@ class Bundler:
         return True
 
 
+def bundle_all(data_dir):
+    vehicle_dir = data_dir / 'getvehicles'
+    count = 0
+    for day_ in vehicle_dir.glob('20??????'):
+        bundler = Bundler(data_dir, day=day_.name)
+        if bundler.scan_day():
+            count += 1
+    print(f'Bundled {count} days')
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Package scraped data.')
     parser.add_argument('--show_stats', action='store_true',
@@ -96,7 +127,12 @@ if __name__ == "__main__":
                         help='Data directory with scraped files')
     args = parser.parse_args()
     data_dir = Path(args.data_dir).expanduser()
-    day = args.day
-    b = Bundler(data_dir, day)
-    b.scan_day()
+    if args.day:
+        day = args.day
+        b = Bundler(data_dir, day)
+        b.scan_day()
+    else:
+        print(f'Bundling all files')
+        bundle_all(data_dir)
+
 

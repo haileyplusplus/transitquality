0ffb7b9132ed9770ae6d3fc0aa270d0e2e8c785d
hailey <188331+haileyplusplus@users.noreply.github.com>
2025-01-06 14:36:09 -0600

    Quick hack for specifying bucket

--- backend/s3client.py
+++ backend/s3client.py
@@ -3,13 +3,16 @@ import datetime
 import boto3
 import sys
 import json
+import os
 
 
 class S3Client:
-    BUCKET_NAME = 'transitquality2024'
-
     def __init__(self):
         self.client = boto3.client('s3')
+        self.bucket_name = 'transitquality2024'
+        if os.getenv('BUCKET'):
+            self.bucket_name = os.getenv('BUCKET')
+        print(f'Using bucket {self.bucket_name}')
         #self.s3 = boto3.resource('s3')
         #self.client = self.s3.Client()
         #self.bucket = self.s3.Bucket('transitquality2024')
@@ -19,7 +22,7 @@ class S3Client:
         full = ts.strftime('%H%M%Sz')
         key = f'bustracker/raw/{command}/{day}/t{full}.json'
         response = self.client.put_object(
-            Bucket=self.BUCKET_NAME,
+            Bucket=self.bucket_name,
             Body=contents.encode('utf-8'),
             Key=key
         )

f44af157847694fb881414201b8cbe702a9191aa
hailey <188331+haileyplusplus@users.noreply.github.com>
2025-01-10 16:50:17 -0600

    Add autocomplete frontend sample

---
+++ frontend/autocompletedev/src/pages/index.vue
@@ -0,0 +1,39 @@
+<template>
+  Main vue<br>
+  Str: {{ hellostr }}
+</template>
+
+<script setup>
+  import { ref } from 'vue';
+
+  const hellostr = ref('Hello!')
+
+  function setPosition(position) {
+    console.log('position is ', position)
+    hellostr.value = '' + position.coords.latitude + ', ' + position.coords.longitude
+  }
+
+  function setHello() {
+    if (navigator.geolocation) {
+      //const position =
+      console.log('position')
+      navigator.geolocation.getCurrentPosition(setPosition)
+      console.log('done')
+    } else {
+      hellostr.value = 'no location available'
+      console.log('no position')
+    }
+  }
+
+  setHello()
+</script>
+
+<script>
+export default {
+  data() {
+    return {
+      hellostr: 'Hello2 !'
+    }
+  },
+}
+</script>

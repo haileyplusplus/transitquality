650e2dcc4365f5f0700daf369a7f5c0ef085f45e
hailey <188331+haileyplusplus@users.noreply.github.com>
2025-01-17 20:27:18 -0600

    Add schedule manager and train view utility

---
+++ bundler/schedule_manager.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python3
+import json
+
+# https://www.transitchicago.com/downloads/sch_data/
+
+from s3path import S3Path
+
+import requests
+import tempfile
+import datetime
+from dateutil.parser import parse
+
+
+class ScheduleManager:
+    BUCKET = S3Path('/transitquality2024/schedules/cta')
+    URL = 'https://www.transitchicago.com/downloads/sch_data/google_transit.zip'
+
+    def __init__(self):
+        self.most_recent = self.most_recent_schedule()
+        self.state = None
+        self.read_state()
+
+    def most_recent_schedule(self):
+        #path = self.BUCKET / '*.zip'
+        schedules = list(self.BUCKET.glob('cta_gtfs_20??????.zip'))
+        if not schedules:
+            return None
+        schedules.sort()
+        try:
+            return datetime.datetime.strptime(schedules[-1].name, 'cta_gtfs_%Y%m%d.zip')
+        except ValueError:
+            return None
+
+    def read_state(self):
+        state = self.BUCKET / 'state.json'
+        if not state.exists():
+            return False
+        self.state = json.loads(state.read_text(encoding='utf-8'))
+        return True
+
+    def write_state(self):
+        state = self.BUCKET / 'state.json'
+        state.write_text(json.dumps(self.state), encoding='utf-8')
+
+    def retrieve(self):
+        needs_update = self.poll()
+        if not needs_update:
+            return
+        with tempfile.TemporaryDirectory as tfh:
+            resp = requests.get(self.URL, stream=True)
+            if resp.status_code != 200:
+                return False
+            try:
+                dt = parse(resp.headers.get('Last-Modified'))
+            except ValueError:
+                return False
+            outname = dt.strftime('cta_gtfs_%Y%m%d.zip')
+            outpath = self.BUCKET / outname
+            for chunk in resp.iter_content(chunk_size=16384):
+                tfh.write(chunk)
+            tfh.seek(0)
+            outpath.write_bytes(tfh.read())
+            self.state = resp.headers
+        self.write_state()
+
+    def poll(self):
+        h = requests.head(self.URL)
+        try:
+            #dt = parse(h.headers.get('Last-Modified'))
+            tag = h.headers['ETag']
+            needs_update = True
+            if self.state and self.state['ETag'] == tag:
+                needs_update = False
+            #self.state = h.headers
+            return needs_update
+        except ValueError:
+            return False
--- tools/traindf.py
+++ tools/traindf.py
@@ -1,12 +1,12 @@
 from pathlib import Path
 import sys
 import json
+import glob
 
 import pandas as pd
 
 
-def read_file(filename: Path):
-    d = {}
+def read_file(filename: Path, dx):
     with open(filename) as fh:
         d = json.load(fh)
         reqs = d['requests']
@@ -14,16 +14,18 @@ def read_file(filename: Path):
             routes = req['response']['ctatt']['route']
             for r in routes:
                 key = r['@name']
-                wdf = d.setdefault(key, pd.DataFrame())
+                wdf = dx.setdefault(key, pd.DataFrame())
                 if 'train' in r:
                     if isinstance(r['train'], dict):
                         ndf = pd.DataFrame([r['train']])
                     else:
                         ndf = pd.DataFrame(r['train'])
-                    d[key] = pd.concat([wdf, ndf], ignore_index=True)
-    return d
+                    dx[key] = pd.concat([wdf, ndf], ignore_index=True)
 
 
 if __name__ == "__main__":
-    filename = Path(sys.argv[1])
-    df = read_file(filename)
+    d = {}
+    #for f in sys.argv[1:]:
+    for f in glob.glob('*.json'):
+        filename = Path(f)
+        read_file(filename, d)

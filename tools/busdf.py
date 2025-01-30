681e30e3d1893e5cd28a063fbefe078dab9b9990
hailey <188331+haileyplusplus@users.noreply.github.com>
2025-01-29 23:09:24 -0600

    Implement validation

--- tools/busdf.py
+++ tools/busdf.py
@@ -7,23 +7,40 @@ import pandas as pd
 from bundler.bundlereader import BundleReader
 from pathlib import Path
 
-TRAIN_ROUTES = ['red', 'p', 'y', 'blue', 'pink', 'g', 'org', 'brn']
 
+class DfManager:
+    TRAIN_ROUTES = ['red', 'p', 'y', 'blue', 'pink', 'g', 'org', 'brn']
+    EXPECTED_UNIQUE = ['vid', 'pid', 'rt', 'des', 'tatripid', 'stst', 'stsd']
 
-def read(bundle_file: Path):
-    reader = BundleReader(bundle_file, routes=None)
-    reader.process_bundle_file()
-    df = pd.DataFrame()
-    for route, vid, vdf in reader.generate_vehicles():
-        if route.route in TRAIN_ROUTES:
-            continue
-        df = pd.concat([df, vdf])
-    return df
+    def __init__(self, bundle_file: Path):
+        reader = BundleReader(bundle_file, routes=None)
+        reader.process_bundle_file()
+        self.df = pd.DataFrame()
+        self.trips_with_errors = set([])
+        for route, vid, vdf in reader.generate_vehicles():
+            if route.route in self.TRAIN_ROUTES:
+                continue
+            self.df = pd.concat([self.df, vdf])
 
+    def record_error(self, trip, msg):
+        self.trips_with_errors.add(trip)
+        print(f'Error in {trip}: {msg}')
 
-def validate(df):
-    pass
+    def validate(self, origtatripno: str):
+        tdf = self.df[self.df.origtatripno == origtatripno]
+        for field in self.EXPECTED_UNIQUE:
+            if tdf[field].nunique() != 1:
+                self.record_error(origtatripno, f'Non-unique field {field}')
+        if not tdf.tmstmp.is_monotonic_increasing:
+            self.record_error(origtatripno, 'tmstmp not monotonically increasing')
+        if not tdf.pdist.is_monotonic_increasing:
+            self.record_error(origtatripno, 'pdist not monotonically increasing')
 
+    def validate_all(self):
+        trips = self.df.origtatripno.unique()
+        for t in trips:
+            self.validate(t)
+        print(f'Found {len(self.trips_with_errors)} trips with errors')
 
 
 if __name__ == "__main__":
@@ -31,4 +48,6 @@ if __name__ == "__main__":
     parser.add_argument('--bundle_file', type=str,
                         help='Bundle filename')
     args = parser.parse_args()
-    df = read(Path(args.bundle_file).expanduser())
+    manager = DfManager(Path(args.bundle_file).expanduser())
+    df = manager.df
+    manager.validate_all()

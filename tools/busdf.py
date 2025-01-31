390ab8152a4273a957510a816ed98611a5639aac
hailey <188331+haileyplusplus@users.noreply.github.com>
2025-01-30 21:36:10 -0600

    Validation updates

--- tools/busdf.py
+++ tools/busdf.py
@@ -6,6 +6,7 @@ import pandas as pd
 
 from bundler.bundlereader import BundleReader
 from pathlib import Path
+from tqdm import tqdm
 
 
 class DfManager:
@@ -17,14 +18,16 @@ class DfManager:
         reader.process_bundle_file()
         self.df = pd.DataFrame()
         self.trips_with_errors = set([])
+        self.errorfile = open('/tmp/errors', 'w')
         for route, vid, vdf in reader.generate_vehicles():
             if route.route in self.TRAIN_ROUTES:
                 continue
-            self.df = pd.concat([self.df, vdf])
+            self.df = pd.concat([self.df, vdf], ignore_index=True)
 
     def record_error(self, trip, msg):
         self.trips_with_errors.add(trip)
-        print(f'Error in {trip}: {msg}')
+        #print(f'Error in {trip}: {msg}')
+        self.errorfile.write(f'Error in {trip}: {msg}\n')
 
     def validate(self, origtatripno: str):
         tdf = self.df[self.df.origtatripno == origtatripno]
@@ -33,15 +36,37 @@ class DfManager:
                 self.record_error(origtatripno, f'Non-unique field {field}')
         if not tdf.tmstmp.is_monotonic_increasing:
             self.record_error(origtatripno, 'tmstmp not monotonically increasing')
-        if not tdf.pdist.is_monotonic_increasing:
+        rows = tdf.pdist.diff().dropna()
+        if rows.empty:
+            self.record_error(origtatripno, 'Not enough rows')
+            return
+        diff = min(rows)
+        #if not tdf[tdf.pdist != tdf.pdist.max()].pdist.is_monotonic_increasing:
+        if diff < -1500:
             self.record_error(origtatripno, 'pdist not monotonically increasing')
+        maxdist = tdf.pdist.max()
+        if maxdist < 4000:
+            self.record_error(origtatripno, f'trip too short: {maxdist}')
 
     def validate_all(self):
         trips = self.df.origtatripno.unique()
-        for t in trips:
+        print(f'Validating {len(trips)} trips.')
+        for t in tqdm(trips):
             self.validate(t)
         print(f'Found {len(self.trips_with_errors)} trips with errors')
+        self.errorfile.close()
 
+    def get_trip(self, origtatripno):
+        tdf = self.df[self.df.origtatripno == origtatripno]
+        return tdf
+
+"""
+Validation cleanups:
+
+Be lenient about end distance reversal
+omit small threshold of stray points
+
+"""
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Read bus updates from a bundle into a large dataframe.')

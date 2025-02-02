80a4b606194d87b6557886fc3356620192e179d5
hailey <188331+haileyplusplus@users.noreply.github.com>
2025-02-02 16:00:26 -0600

    Add helpers

---
+++ motis/localimport.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# https://stackoverflow.com/questions/59895/how-do-i-get-the-directory-where-a-bash-script-is-located-from-within-the-script
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+cat $SCRIPT_DIR/motis-config.yaml | sed "s/\/data\///" > config.yaml

3634de944e5bf23d4dc3641bf298aabd431d37fa
hailey <188331+haileyplusplus@users.noreply.github.com>
2025-02-03 12:10:47 -0600

    Fix time zone settings

--- realtime/queries.py
+++ realtime/queries.py
@@ -14,6 +14,7 @@ from pydantic import BaseModel
 
 
 from realtime.rtmodel import db_init, BusPosition, CurrentVehicleState, Stop
+from backend.util import Util
 
 
 class StopEstimate(BaseModel):
@@ -117,7 +118,7 @@ class QueryManager:
 
     def get_position_dataframe(self, pid):
         with Session(self.engine) as session:
-            thresh = datetime.datetime.now() - datetime.timedelta(hours=5)
+            thresh = datetime.datetime.now(tz=Util.CTA_TIMEZONE) - datetime.timedelta(hours=5)
             # select timestamp, pdist, origtatripno from bus_position where pid = 5907 order by origtatripno, timestamp;
             query = select(BusPosition).where(BusPosition.pid == pid).where(
                 BusPosition.timestamp > thresh).order_by(

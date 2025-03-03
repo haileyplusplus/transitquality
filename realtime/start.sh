999a306ee846738930b22cee21bb7c06166e2746
hailey <188331+haileyplusplus@users.noreply.github.com>
2025-03-02 22:23:51 -0600

    Fix bug

--- realtime/start.sh
+++ realtime/start.sh
@@ -3,7 +3,7 @@ HOST=$(hostname -s)
 if [[ "$HOST" == "brie" ]]
 then
   echo "Starting with prod config on $HOST"
-  docker compose -f compose.yaml -f compose.dev.yaml up --build rtserver -d
+  docker compose -f compose.yaml -f compose.prod.yaml up --build rtserver -d
 else
   echo "Starting with dev config on $HOST"
   docker compose -f compose.yaml -f compose.dev.yaml up --build rtserver "$@"

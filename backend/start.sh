3174475577982a4d7ff14156464d3f1be53607f2
hailey <188331+haileyplusplus@users.noreply.github.com>
2025-03-15 17:11:34 -0500

    Add environment-specific configs for backend

---
+++ backend/start.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+HOST=$(tailscale status --json | jq .Self.DNSName | tr -d \" | cut -d\. -f 1)
+if [[ "$HOST" == "leonard" ]]
+then
+  echo "Starting with prod config on $HOST"
+  docker compose -f compose.yaml -f compose.leonard.yaml up --build -d
+elif [[ "$HOST" == "citycollege" ]]
+then
+  echo "Starting with prod config on $HOST"
+  docker compose -f compose.yaml -f compose.citycollege.yaml up --build -d
+else
+  echo "Starting with dev config on $HOST"
+  docker compose -f compose.yaml -f compose.dev.yaml up --build "$@"
+fi
